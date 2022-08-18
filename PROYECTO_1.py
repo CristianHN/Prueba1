2bc4685c9fdf4bc5ad11adeddf85e34328775f51
Cristian Hernan Niño Ortiz <chnino7@misena.edu.co>
2022-08-17 21:50:10 -0500

    Este es un ejemplo de actualizacion de archivos de java y es mi primer cargue de archivos a GitHub

---
+++ PROYECTO_1.py
@@ -0,0 +1,18 @@
+#encontrar el valor de z si si X=5 y=15 
+
+#salto de linea sep='\n'
+print ('hola')
+nombre = input('escriba un nombre:')
+print(nombre)
+
+x =float(input('escribe numero x: '))
+y = float(input('escribe numero y: '))
+
+z=x+y
+print(' z equivale a:',z)
+
+
+
+
+ 
+ 
